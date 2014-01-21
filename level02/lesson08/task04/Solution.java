88df2bd22d0df308fde98aceccf4d660f096075c
Anastasia Grineva <grint@inbox.ru>
2014-01-21 06:28:06 -0800

    Update Solution.java

--- level02/lesson08/task04/Solution.java
+++ level02/lesson08/task04/Solution.java
@@ -4,21 +4,17 @@ package com.javarush.test.level02.lesson08.task04;
 Написать функцию, которая вычисляет минимум из четырёх чисел.
 Использовать функцию из задачи 1.
 */
-public class Solution
-{
-    public static int min(int a, int b, int c, int d)
-    {
+public class Solution {
+    public static int min(int a, int b, int c, int d) {
         return min(min(a, b), min(c, d));
 
     }
 
-    public static int min(int a, int b)
-    {
+    public static int min(int a, int b) {
         if (a < b) {
             return a;
         } else {
             return b;
         }
-
     }
 }
