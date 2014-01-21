115f8545fb09732d4ba4a012c29e3c8c2f57c06b
Anastasia Grineva <grint@inbox.ru>
2014-01-21 06:33:04 -0800

    Update Solution.java

--- level03/lesson08/task04/Solution.java
+++ level03/lesson08/task04/Solution.java
@@ -9,14 +9,11 @@ name1 проспонсировал name2 и она стала известной
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
+public class Solution {
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String name1 = reader.readLine();
         String name2 = reader.readLine();
         System.out.println(name1+" проспонсировал "+name2+", и она стала известной певицей.");
-
     }
 }
