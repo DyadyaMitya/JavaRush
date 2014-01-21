36a1d0f896bb784dde22961a569311e1990beec0
Anastasia Grineva <grint@inbox.ru>
2014-01-21 06:26:49 -0800

    Update Solution.java

--- level02/lesson08/task02/Solution.java
+++ level02/lesson08/task02/Solution.java
@@ -3,16 +3,13 @@ package com.javarush.test.level02.lesson08.task02;
 /* Максимум двух чисел
 Написать функцию, которая вычисляет максимум из двух чисел.
 */
-public class Solution
-{
-    public static int max(int a, int b)
-    {
+public class Solution {
+    public static int max(int a, int b) {
         //Напишите тут ваш код
         if (a > b) {
             return a;
         } else {
             return b;
         }
-
     }
 }
