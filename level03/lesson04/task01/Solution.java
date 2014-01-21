8eaa744eaaefcb58a6b789a21cf19fd5c4b47da2
Anastasia Grineva <grint@inbox.ru>
2014-01-21 06:29:04 -0800

    Update Solution.java

--- level03/lesson04/task01/Solution.java
+++ level03/lesson04/task01/Solution.java
@@ -4,12 +4,9 @@ package com.javarush.test.level03.lesson04.task01;
 Вывести на экран дату своего рождения в виде: MAY 1 2012
 */
 
-public class Solution
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+    public static void main(String[] args) {
         String date = new String("MAY 1 2012");
         System.out.println(date);
-        
     }
 }
