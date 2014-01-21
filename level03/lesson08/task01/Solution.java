c3afbd6597a6357fd966b1774ac7673e968d779a
Anastasia Grineva <grint@inbox.ru>
2014-01-21 06:32:35 -0800

    Update Solution.java

--- level03/lesson08/task01/Solution.java
+++ level03/lesson08/task01/Solution.java
@@ -9,15 +9,11 @@ package com.javarush.test.level03.lesson08.task01;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
+public class Solution {
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
         String time = reader.readLine();
         String name = reader.readLine();
         System.out.println(name + " захватит мир через " + time + " лет. Му-ха-ха!");
-
     }
 }
