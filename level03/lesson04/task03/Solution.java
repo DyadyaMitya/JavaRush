3b383e0e9daf94cbe962281444f2446c78703484
Anastasia Grineva <grint@inbox.ru>
2014-01-21 06:29:46 -0800

    Update Solution.java

--- level03/lesson04/task03/Solution.java
+++ level03/lesson04/task03/Solution.java
@@ -4,11 +4,8 @@ package com.javarush.test.level03.lesson04.task03;
 Вывести на экран 'Бендер мой любимый учитель' 1 раз.
 */
 
-public class Solution
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+    public static void main(String[] args) {
        System.out.println("Бендер мой любимый учитель");
-
     }
 }
